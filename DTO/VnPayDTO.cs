79d45dbc9a3ce3a7ca17da59421808c1601e5d0d
Hao Pham <Hao.bestzed@gmail.com>
2023-10-14 15:05:47 +0700

    DTO

---
+++ DTO/VnPayDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace QLSB_APIs.DTO
+{
+    public class VnPayDTO
+    {
+        public string Url { get; set; }
+    }
+
+    public class VnPayResultDTO
+    {
+        public string vnp_SecureHash { get; set; }
+        public string vnp_TransactionStatus { get; set; }
+        public string vnp_ResponseCode { get; set; }
+        public int vnp_TxnRef { get; set; }
+
+    }
+
+    public class ResultDTO
+    {
+        public string message { get; set; }
+
+    }
+}
